3a912aceb4a6c066266e65cc881adf8d52b63d8c
Juan <josemariacosta_8@hotmail.com>
2025-05-10 20:50:41 -0500

    Added .env functionality

--- app.py
+++ app.py
@@ -9,8 +9,26 @@ from PIL import Image
 import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("firebase_credentials.json")
+load_dotenv()
+
+firebase_config = {
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(firebase_config)
 
 firebase_admin.initialize_app(cred)
 
