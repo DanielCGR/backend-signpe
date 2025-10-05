e4066e5c08b770c39540abe17c37dd1c58f9f7c8
Juan <josemariacosta_8@hotmail.com>
2025-10-04 20:30:55 -0500

    fix: updated cors

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ except Exception as e:
 db = firestore.client()
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173","https://signpe-lo8uskfq6-chorris-projects.vercel.app"])
+CORS(app, origins=["http://localhost:5173","https://signpe-lo8uskfq6-chorris-projects.vercel.app","https://signpe.vercel.app/"])
 
 # Inicializar MediaPipe
 mp_pose = mp.solutions.pose
